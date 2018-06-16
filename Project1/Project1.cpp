18e2aaacd37a53a5db324dfdb9edff805721df46
kunalJa <kunaljain@utexas.edu>
2018-06-15 19:44:45 -0500

    Implemented dictionary look up and comparison and comments

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -27,37 +27,43 @@
  * almost certainly find it easiest to just write everything you need from scratch!
  */
 
-
-typedef struct word_t {
-    char* word_ptr;
+typedef struct word_t { // Struct to hold my found words for printing and comparing to the dictionary
+    char* word_ptr;     // Pointer to where in the article the first letter of the word can be found
     int length;
 } word;
 
 int isALetter(char c);
 int inDictionary(word w, char dictionary[]);
+char toUpperCase(char c);
 
+
+// Prints to console words that are not in the known (passed in) dictionary- these may be miss-spelled
 void spellCheck(char article[], char dictionary[]) {
     int currentCharIndex = 0;
-    int wordStart = 0;
-    int wordFound = 0;
+    int wordStart = 0;  // holds the index of the start of the potential word
+    int wordFound = 0;  // a flag for when a potential word is being measured
     char currentChar = article[currentCharIndex];
 
     while(currentChar != 0){
-        if(isALetter(currentChar) && wordFound == 0){
+        if(isALetter(currentChar) && wordFound == 0){ // current char is a letter = this may potentially be a word
             wordFound = 1;
             currentCharIndex++;
         }
+
         else if(isALetter(currentChar) && wordFound == 1){
             currentCharIndex++;
         }
+
         else if(!isALetter(currentChar) && wordFound == 0){
             currentCharIndex++;
             wordStart++;
         }
+
         else{
             int wordLength = currentCharIndex - wordStart;
             if (wordLength > 1){
-                word current = {&(article[wordStart]), wordLength};
+                word current = {&(article[wordStart]), wordLength}; // placing our found word into the struct
+
                 if (!inDictionary(current, dictionary)){
                     for(int i = 0; i < current.length; i++){
                         printf("%c", current.word_ptr[i]);
@@ -86,10 +92,54 @@ void spellCheck(char article[], char dictionary[]) {
     }
 }
 
+// Checks whether or not char c is a letter in the English alphabet
 int isALetter(char c){
     return (((c >= 'A') && (c <= 'Z')) || ((c >= 'a') && (c<= 'z')));
 }
 
+// compares a known word to the words in the passed in dictionary
+// returns 1 if it is found in the dictionary
+// returns 0 otherwise
 int inDictionary(word w, char dictionary[]){
+    int dictInd = 0;
+    int correctNumLetters = 0;
+    char nextWord = '\n';
+    while(dictionary[dictInd] != 0){
+        for (int i = 0; i < w.length; i++){
+            if(dictionary[dictInd] == nextWord || dictionary[dictInd] == 0){ // break if reached the end of the dictionary word
+                break;
+            }
+
+            if(toUpperCase(w.word_ptr[i]) == toUpperCase(dictionary[dictInd])){
+                correctNumLetters++;
+            }
+            dictInd++;
+        }
+
+        if (correctNumLetters == w.length && (dictionary[dictInd] == nextWord || dictionary[dictInd] == 0)){
+            return 1;   // the words match only when they are the same length and have all the same letters
+        }
+
+        while(dictionary[dictInd] != nextWord){ // moves the pointer to the next word in the dictionary
+            if(dictionary[dictInd] == 0){
+                break;
+            }
+            dictInd++;
+        }
+
+        if(dictionary[dictInd] == nextWord){ // increments the dictionary index pointer to the beginning of the next word
+            dictInd++;
+        }
+
+        correctNumLetters = 0;
+    }
     return 0;
+}
+
+// Converts a character, if its a lowercase letter, to an uppercase letter and returns it
+char toUpperCase(char c){
+    if ((c >= 'a') && (c<= 'z')){
+        return c + ('A' - 'a');
+    }
+    return c;
 }
