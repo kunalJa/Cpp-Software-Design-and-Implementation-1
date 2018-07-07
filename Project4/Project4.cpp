289e1dc7b649dd31d393bca9f80acd8f37f40336
kunalJa <kunaljain@utexas.edu>
2018-07-07 13:58:52 -0500

    Project4 due Wednesday

---
+++ Project4/Project4.cpp
@@ -0,0 +1,29 @@
+// CRM <Project4.cpp>
+// EE 312 Project 4 submission by
+// Kunal Jain
+// ksj572
+// Slip days used: 0
+// Summer 2018
+
+#include <stdio.h>
+#include <assert.h>
+#include "MyString.h"
+#include "Invent.h"
+
+#define MAX_CUSTOMERS 1000
+int num_customers = 0;
+Customer customers[MAX_CUSTOMERS];
+
+Customer inventory;
+
+
+/* clear the inventory and reset the customer database to empty */
+void reset(void) {
+	/* your code here */
+}
+
+void processSummarize() {}
+
+void processPurchase() {}
+
+void processInventory() {}
