f28c2f9158bcdc7742fdc1a9a51909bffad19ef1
kunalJa <kunaljain@utexas.edu>
2018-07-09 21:45:09 -0500

    Refactor inventory name conflict

--- Project4/Project4.cpp
+++ Project4/Project4.cpp
@@ -16,7 +16,7 @@ int bottlesPurchased = 0;
 int rattlesPurchased = 0;
 int diapersPurchased = 0;
 Customer customers[MAX_CUSTOMERS];
-Customer inventory = {0, 0, 0, 0};
+Customer myInventory = {{10, "inventory"}, 0, 0, 0};
 String bottles = StringCreate("Bottles");
 String rattles = StringCreate("Rattles");
 String diapers = StringCreate("Diapers");
@@ -28,7 +28,7 @@ void customerZero(Customer* c) {
     c->diapers = 0;
 }
 
-// Makes sure the item requested is both in the inventory and in the volume requested
+// Makes sure the item requested is both in the myInventory and in the volume requested
 int inStock(String* Name, String* itemName, int itemAmount) {
     int which;
     int outOfStock;
@@ -38,13 +38,13 @@ int inStock(String* Name, String* itemName, int itemAmount) {
     }
 
     if (StringIsEqualTo(&bottles, itemName)) {
-        outOfStock = inventory.bottles < itemAmount ? 1 : 0;
+        outOfStock = myInventory.bottles < itemAmount ? 1 : 0;
         which = 0;
     } else if (StringIsEqualTo(&diapers, itemName)) {
-        outOfStock = inventory.diapers < itemAmount ? 1 : 0;
+        outOfStock = myInventory.diapers < itemAmount ? 1 : 0;
         which = 1;
     } else if (StringIsEqualTo(&rattles, itemName)) {
-        outOfStock = inventory.rattles < itemAmount ? 1 : 0;
+        outOfStock = myInventory.rattles < itemAmount ? 1 : 0;
         which = 2;
     } else {
         return 0;
@@ -54,11 +54,11 @@ int inStock(String* Name, String* itemName, int itemAmount) {
         printf("Sorry ");
         StringPrint(Name);
         if (which == 0) {
-            printf(", we only have %d ", inventory.bottles);
+            printf(", we only have %d ", myInventory.bottles);
         } else if (which == 1) {
-            printf(", we only have %d ", inventory.diapers);
+            printf(", we only have %d ", myInventory.diapers);
         } else {
-            printf(", we only have %d ", inventory.rattles);
+            printf(", we only have %d ", myInventory.rattles);
         }
         StringPrint(itemName);
         printf("\n");
@@ -68,9 +68,9 @@ int inStock(String* Name, String* itemName, int itemAmount) {
     return 1;
 }
 
-// Clear the inventory and reset the customer database to empty
+// Clear the myInventory and reset the customer database to empty
 void reset() {
-    customerZero(&inventory);
+    customerZero(&myInventory);
 
     for (int i = 0; i < num_customers; i++) {
         StringDestroy(&customers[i].name);
@@ -83,11 +83,10 @@ void reset() {
 }
 
 // Process a Summarize instruction
-// Prints a summary of current customers and inventory
+// Prints a summary of current customers and myInventory
 void processSummarize() {
-    printf("There are %d Bottles, %d Diapers and %d Rattles in inventory \n", inventory.bottles,
-           inventory.diapers, inventory.rattles);
-    printf("we have had a total of %d different customers", num_customers);
+    printf("There are %d Bottles, %d Diapers and %d Rattles in inventory\n", myInventory.bottles, myInventory.diapers, myInventory.rattles);
+    printf("we have had a total of %d different customers\n", num_customers);
 
     if (bottlesPurchased) {
         int maxBottles = 0;
@@ -97,9 +96,9 @@ void processSummarize() {
             }
         }
         StringPrint(&customers[maxBottles].name);
-        printf(" has purchased the most Bottles (%d)", customers[maxBottles].bottles);
+        printf(" has purchased the most Bottles (%d)\n", customers[maxBottles].bottles);
     } else {
-        printf("no one has purchased any Bottles");
+        printf("no one has purchased any Bottles\n");
     }
 
     if (diapersPurchased) {
@@ -110,9 +109,9 @@ void processSummarize() {
             }
         }
         StringPrint(&customers[maxDiapers].name);
-        printf(" has purchased the most Diapers (%d)", customers[maxDiapers].diapers);
+        printf(" has purchased the most Diapers (%d)\n", customers[maxDiapers].diapers);
     } else {
-        printf("no one has purchased any Diapers");
+        printf("no one has purchased any Diapers\n");
     }
 
     if (rattlesPurchased) {
@@ -123,9 +122,9 @@ void processSummarize() {
             }
         }
         StringPrint(&customers[maxRattles].name);
-        printf(" has purchased the most Rattles (%d)", customers[maxRattles].rattles);
+        printf(" has purchased the most Rattles (%d)\n", customers[maxRattles].rattles);
     } else {
-        printf("no one has purchased any Rattles");
+        printf("no one has purchased any Rattles\n");
     }
 }
 
@@ -134,61 +133,61 @@ void processSummarize() {
 void processPurchase() {
     int customerIndex = num_customers;
 
-    String* Name;
-    String* itemName;
+    String Name;
+    String itemName;
     int itemAmount;
-    readString(Name);
-    readString(itemName);
+    readString(&Name);
+    readString(&itemName);
     readNum(&itemAmount);
 
-    if (inStock(Name, itemName, itemAmount)) {
+    if (inStock(&Name, &itemName, itemAmount)) {
         for (int i = 0; i < num_customers; i++) {
-            if (StringIsEqualTo(Name, &customers[i].name)) {
+            if (StringIsEqualTo(&Name, &customers[i].name)) {
                 customerIndex = i;
                 break;
             }
         }
 
         if (customerIndex == num_customers) {
-            StringReAssign(&customers[customerIndex].name, Name);
+            StringReAssign(&customers[customerIndex].name, &Name);
             customerZero(&customers[customerIndex]);
             num_customers++;
         }
 
-        if (StringIsEqualTo(&bottles, itemName)) {
+        if (StringIsEqualTo(&bottles, &itemName)) {
             customers[customerIndex].bottles += itemAmount;
-            inventory.bottles -= itemAmount;
+            myInventory.bottles -= itemAmount;
             bottlesPurchased = 1;
-        } else if (StringIsEqualTo(&diapers, itemName)) {
+        } else if (StringIsEqualTo(&diapers, &itemName)) {
             customers[customerIndex].diapers += itemAmount;
-            inventory.diapers -= itemAmount;
+            myInventory.diapers -= itemAmount;
             diapersPurchased = 1;
-        } else if (StringIsEqualTo(&rattles, itemName)) {
+        } else if (StringIsEqualTo(&rattles, &itemName)) {
             customers[customerIndex].rattles += itemAmount;
-            inventory.rattles -= itemAmount;
+            myInventory.rattles -= itemAmount;
             rattlesPurchased = 1;
         }
     }
 
-    StringDestroy(Name);
-    StringDestroy(itemName);
+    StringDestroy(&Name);
+    StringDestroy(&itemName);
 }
 
 // Process an Inventory instruction
-// Add an item to the inventory
+// Add an item to the myInventory
 void processInventory() {
-    String* itemName;
+    String itemName;
     int itemAmount;
-    readString(itemName);
+    readString(&itemName);
     readNum(&itemAmount);
 
-    if (StringIsEqualTo(&bottles, itemName)) {
-        inventory.bottles += itemAmount;
-    } else if (StringIsEqualTo(&diapers, itemName)) {
-        inventory.diapers += itemAmount;
-    } else if (StringIsEqualTo(&rattles, itemName)) {
-        inventory.rattles += itemAmount;
+    if (StringIsEqualTo(&bottles, &itemName)) {
+        myInventory.bottles += itemAmount;
+    } else if (StringIsEqualTo(&diapers, &itemName)) {
+        myInventory.diapers += itemAmount;
+    } else if (StringIsEqualTo(&rattles, &itemName)) {
+        myInventory.rattles += itemAmount;
     }
 
-    StringDestroy(itemName);
+    StringDestroy(&itemName);
 }
