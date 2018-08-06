60e3b7b52cca966a4d5f190a55963a275861e9db
kunalJa <kunaljain@utexas.edu>
2018-08-06 17:03:18 -0500

    Project 8 starter files

---
+++ Project8/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Parse.h"
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+void run() {
+
+}
+
+int main() {
+    set_input("test_grader.blip");
+    run();
+}
