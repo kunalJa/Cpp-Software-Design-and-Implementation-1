af2e779ce20c9c985098faf3a80e10342bc6be9e
kunalJa <kunaljain@utexas.edu>
2018-08-13 17:20:15 -0500

    blip test files

--- Project9/main.cpp
+++ Project9/main.cpp
@@ -143,21 +143,21 @@ void run() {
 
 
 int main(void) {
-//    set_input("test1.blip");
-//    run();
-//    cout << endl;
-//    set_input("test2.blip");
-//    run();
-//    cout << endl;
-//    set_input("test3.blip");
-//    run();
-//    cout << endl;
-//    set_input("test4.blip");
-//    run();
-//    cout << endl;
-//    set_input("test5.blip");
-//    run();
+    set_input("test1.blip");
+    run();
     cout << endl;
+    set_input("test2.blip");
+    run();
+    cout << endl;
+    set_input("test3.blip");
+    run();
+////    cout << endl;
+////    set_input("test4.blip");
+////    run();
+////    cout << endl;
+////    set_input("test5.blip");
+////    run();
+//    cout << endl;
     set_input("test6.blip");
     run();
     cout << endl;
@@ -170,6 +170,6 @@ int main(void) {
 //    set_input("test9.blip");
 //    run();
 //    cout << endl;
-//    set_input("test10.blip");
-//    run();
+    set_input("test10.blip");
+    run();
 }
