2c930be66c225445b311255ccfc2f6f9943cbddf
Kunal <kunaljain@utexas.edu>
2018-11-17 16:30:57 -0600

    Update Project5.cpp

--- Project5/Project5.cpp
+++ Project5/Project5.cpp
@@ -1,10 +1,3 @@
-// Set <Project5.cpp>
-// EE 312 Project 5 submission by
-// Kunal Jain
-// ksj572
-// Slip days used: 0
-// Summer 2018
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
