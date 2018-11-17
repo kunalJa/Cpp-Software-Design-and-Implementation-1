fb1b4b9566bd19dbb123e97dd54cf2d798b81ec6
Kunal <kunaljain@utexas.edu>
2018-11-17 16:34:14 -0600

    Update main.cpp

--- Project8/main.cpp
+++ Project8/main.cpp
@@ -1,10 +1,3 @@
-// Blip <Project8.cpp>
-// EE 312 Project 8 submission by
-// Kunal Jain
-// ksj572
-// Slip days used: 0
-// Summer 2018
-
 #include <iostream>
 #include <string>
 #include <vector>
