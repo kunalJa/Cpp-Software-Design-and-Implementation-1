feea5b4f969b35e1c53c58a2936eef05aedfe5e3
Kunal <kunaljain@utexas.edu>
2018-11-17 16:30:36 -0600

    Update Project4.cpp

--- Project4/Project4.cpp
+++ Project4/Project4.cpp
@@ -1,10 +1,3 @@
-// CRM <Project4.cpp>
-// EE 312 Project 4 submission by
-// Kunal Jain
-// ksj572
-// Slip days used: 0
-// Summer 2018
-
 #include <stdio.h>
 #include <assert.h>
 #include "MyString.h"
